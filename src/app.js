1f059701beb29ad08247fbae1bdacf3ed2d65ba0
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-06-22 20:32:27 +0700

    building the footer components of netflix --> completed

--- src/app.js
+++ src/app.js
@@ -1,23 +1,12 @@
-import Jumbotron from "./components/jumbotron";
-import jumboData from "./fixtures/jumbo";
+import JumbotronContainer from "./containers/jumbotron";
+import FooterContainer from "./containers/footer";
 
 const App = () => {
   return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => {
-        return (
-          <Jumbotron key={item.id} direction={item.direction}>
-            <Jumbotron.Pane>
-              <Jumbotron.Title>{item.title}</Jumbotron.Title>
-              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-            </Jumbotron.Pane>
-            <Jumbotron.Pane>
-              <Jumbotron.Image src={item.image} alt={item.alt} />
-            </Jumbotron.Pane>
-          </Jumbotron>
-        );
-      })}
-    </Jumbotron.Container>
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
   );
 };
 
