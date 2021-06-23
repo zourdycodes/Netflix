0e7ae68b60f7af8e267f37b25013c16c031e2cf5
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-06-23 13:31:00 +0700

    building the accordion/faq components of netflix --> completed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import JumbotronContainer from "./containers/jumbotron";
 import FooterContainer from "./containers/footer";
+import FaqsContainer from "./containers/faqs";
 
 const App = () => {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
